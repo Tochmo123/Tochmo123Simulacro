015c6261a3baf3cf5b702dea39d6b138059bb741
Tomas Chiva <tochmo@floridauniversitaria.es>
2023-02-11 19:15:50 +0100

    Solucion g.sh

--- asd.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
-
-media=`cat notas.txt | awk '{print $3}' | head -n$i | tail -n1`
-
-echo "$media"
--- g.sh
+++ g.sh
@@ -1,12 +1,10 @@
- #!/bin/bash
-lineas=`cat notas.txt | wc -l`
-lineas2=$((lineas-1))
+#!/bin/bash
+total=`cat notas.txt | tail -n+2 | wc -l`
 suma=0
-for i in `seq 2 $lineas`
-do
-   media=`cat notas.txt | awk '{print $3}' | head -n$i | tail -n1`
+for i in `seq 1 $total`;do
+   media=`cat notas.txt | awk '{print $3}' | head -n$((i+1)) | tail -1`
    suma=$((suma+media))
    echo $suma
 done
-promedio=$((suma/$lineas2))
-echo "La media de todos los modulos es $promedio"
+promedio=$((suma/$total))
+echo "La media de modulos es $promedio"
